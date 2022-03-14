430dcb4c525a12d665acea2a58d33d8b491273a6
Andrew <andrewnijmeh1@gmail.com>
2022-03-14 17:51:02 -0400

    add colors + layout

---
+++ packages/mobile/constants/colors.ts
@@ -0,0 +1,11 @@
+const tintColorLight = "#502196";
+
+export default {
+    light: {
+        text: '#000',
+        background: '#fff',
+        tint: tintColorLight,
+        tabIconDefault: '#ccc',
+        tabIconSelected: tintColorLight,
+    },
+};
