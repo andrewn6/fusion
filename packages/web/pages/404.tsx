7d7e2edfbc159650326034522ae30f689f6853c2
Andrew <andrewnijmeh1@gmail.com>
2022-03-14 17:30:59 -0400

    pre-commits cript

--- packages/web/pages/404.tsx
+++ packages/web/pages/404.tsx
@@ -3,7 +3,8 @@ import type { NextPage } from 'next'
 const NotFound: NextPage = () => {
   return (
     <div>
-      404 Page Not Found | Fusion
+    
+      <h1>404 | Page Not Found</h1>
     </div>
   )
 }
