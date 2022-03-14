75eae90bf20037b090b5b0cd6552a3448bb7a3fd
agn <pineapplerind.info@gmail.com>
2022-03-14 15:51:30 -0400

    tailwind

--- packages/web/components/Signup.tsx
+++ packages/web/components/Signup.tsx
@@ -3,6 +3,9 @@ const Signup: NextPage = () => {
     return (
         <div>
             <h1>Sign up</h1>
+            <form action="/api/v1/users/create">
+                
+            </form>
         </div>
     )
 }
---
+++ packages/web/tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  content: ["./web/**/*.{tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
