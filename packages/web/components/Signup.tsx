b6249d567314363d8d0ef8850696e2abd9468227
agn <pineapplerind.info@gmail.com>
2022-03-14 16:28:18 -0400

    signup page

--- packages/web/components/Signup.tsx
+++ packages/web/components/Signup.tsx
@@ -1,10 +1,15 @@
 import type { NextPage } from 'next'
+
 const Signup: NextPage = () => {
     return (
         <div>
-            <h1>Sign up</h1>
-            <form action="/api/v1/users/create">
-                
+            <h1 className='font-black text-2xl'>Sign up</h1>
+            <form className='p-6 m-2 bg-gray-100 rounded-xl shadow' action="/api/v1/users/create">
+                <input name="username" />
+                <input name="email" />
+                <input name="password" />
+                <input name="confirmPassword" />
+                <button type="submit" value="Sign up"></button>
             </form>
         </div>
     )
