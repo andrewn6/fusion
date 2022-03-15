dbf83c863543893f71a7087c2afe01f71c14eca6
Andrew <andrewnijmeh1@gmail.com>
2022-03-15 15:30:49 -0400

    add hoks folder, and API error

--- packages/api/src/routes/user/route.ts
+++ packages/api/src/routes/user/route.ts
@@ -1,9 +1,10 @@
 import { Static } from "@sinclair/typebox";
-import { FastifyPluginAsync } from "fastify";
+import { FastifyPlugin, FastifyPluginAsync } from "fastify";
 import { RouteShorthandOptions } from "fastify";
 import { User } from "../../schemas/User";
 import { ApiResponseType  } from "../../types/global";
 import { getSucessResponseSchema } from "../../types/success-response-schema";
+
 namespace TB {
     export const ResponseData = User;
     export type responseData = Static<typeof ResponseData>;
@@ -12,3 +13,33 @@ namespace TB {
     export type ResponseType = ApiResponseType<responseData>;
     
 }
+
+namespace Router {
+    const options: RouteShorthandOptions = {
+        schema: {
+            response: { 200: TB.Response}
+        },
+    };
+
+    export const route: FastifyPluginAsync = async (
+        fastify,
+        opts
+    ): Promise<void> => {
+        // TODO: check if they are authencitaced via hook
+        fastify.addHook("preHandler", isAuthenticated);
+
+        fastify.get(
+            "/me",
+            options,
+            async function (req, res): Promise<TB.ResponseType> {
+                res.status(200)
+                return {
+                    ok: true,
+                    message: "Successfully retrieved user!",
+                    data: req.user,
+                };
+            }
+        );
+}
+
+export default Router.route;
---
+++ packages/api/src/utils/errors.ts
@@ -0,0 +1,24 @@
+import { ApiCode } from "~/types/codes";
+
+export async function ApiError (
+    status: number = 500,
+    message: string = 'Internal server error :(',
+    code: ApiCode = "unknown"
+) {
+    throw new Error(`${status} || ${message} ${code}`);
+}
+
+export async function executeOrFail<T>(
+    cb: () => <T> | Promise<T>,
+    status = 500,
+    message = "Internal server error.",
+    code: ApiCode = "unknown"
+) {
+    try {
+        return await cb();
+    } catch(err) {
+        console.log(err);
+        ApiError(status, message, code);
+        return;
+    }
+} 
