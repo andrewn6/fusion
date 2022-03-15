991a55f383b74554a83157b5967ccfcd488c65fb
Andrew <andrewnijmeh1@gmail.com>
2022-03-15 16:11:09 -0400

    user model

--- packages/api/src/models/User.model.ts
+++ packages/api/src/models/User.model.ts
@@ -4,11 +4,29 @@ import { SocialProviderEnum } from "~/enums/SocialProvider.enum";
 
 @Model()
 export class User {
+    // id
     @Property(() => String)
     @Id("uuid")
     id: string;
+    
 
+    // created at
     @Default("now")
     @Property(() => DateTime)
     createdAt: Date;
+
+    // updatedAt
+    @UpdatedAt()
+    @Property(() => DateTime)
+    updatedAt: Date;
+
+    // Username, it's unique.
+    @Unique()
+    @Property(() => String)
+    username: string;
+
+    // Signup method (Discord, twitter etc..)
+    @Property(() => SignupMethodEnum)
+    signupMethod: SignupMethodType;
+
 }
