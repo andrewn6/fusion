78c150793c55556d2f12564ca91d21b71afb7a42
Andrew <andrewnijmeh1@gmail.com>
2022-03-21 17:38:07 -0400

    add token cluster schema

--- packages/api/src/schemas/TokenCluster.ts
+++ packages/api/src/schemas/TokenCluster.ts
@@ -1,2 +1,11 @@
-import { Type } from "@sinclair/typebox";
+import { Static, Type } from "@sinclair/typebox";
 
+export const TokenCluster = Type.Object({
+    id: Type.String(),
+    createdAt: Type.Any(),
+    updatedAt: Type.Any(),
+    accessToken: Type.String(),
+    refreshToken: Type.Optional(Type.String()),
+})
+
+export type TokenClusterType = Static<typeof TokenCluster>;
