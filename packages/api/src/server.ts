136a44c38f45b28e9713298f4e0a462fe2366272
Andrew <andrewnijmeh1@gmail.com>
2022-03-24 21:43:13 -0400

    fix: server.ts failing

--- packages/api/src/server.ts
+++ packages/api/src/server.ts
@@ -1,6 +1,6 @@
 //import { join } from "path";
-import fastify from "fastify";
-//import * as fastifyBlipp from "fastify-blipp";
+import Fastify from "fastify";
+//import* as fastifyBlipp from "fastify-blipp";
 import { Server, IncomingMessage, ServerResponse } from "http";
 import * as dotenv from "dotenv";
 import { logger } from "./providers/logger";
@@ -8,14 +8,15 @@ import { logger } from "./providers/logger";
 const fastify = Fastify();
 const port = Number(process.env.PORT) || 3000;
 
+
 function runServer(): void {
   fastify.listen(port, "0.0.0.0", (err, address) => {
     if (!err) {
-      logger.info(`Server running at ${address}`);
+      logger.info(` Server running @ ${address} 🚀`);
       return;
     }
 
-    logger.error(err.message);
+    console.log(err.message);
     return process.exit(1);
   });
 }
